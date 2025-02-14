ccb52cefac278fdf6e0a7e8e611677395c5c6864
alanbrunoscience <alanbrunomath@gmail.com>
2025-02-14 14:18:16 -0300

    :sparkles: Update Navbar Component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,16 +2,16 @@ function Navbar() {
   return (
     <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
       <div className="container flex justify-between text-lg">
-        <p>Blog Pessoal</p>
+        <p className="ml-4">Blog Pessoal</p>
 
         <div className="flex">
           <nav>
             <ul className="flex text-center list-none">
-              <li>Postagens</li>
-              <li className="ml-4">Temas</li>
-              <li className="ml-4">Cadastrar Tema</li>
-              <li className="ml-4">Perfil</li>
-              <li className="ml-4">Sair</li>
+              <li className="mr-4">Postagens</li>
+              <li className="mr-4">Temas</li>
+              <li className="mr-4">Cadastrar Tema</li>
+              <li className="mr-4">Perfil</li>
+              <li className="mr-4">Sair</li>
             </ul>
           </nav>
         </div>
