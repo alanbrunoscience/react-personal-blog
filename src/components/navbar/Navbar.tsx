7a16eba8c020dd5c9fd3d1be1275642b63d98add
alanbrunoscience <alanbrunomath@gmail.com>
2025-02-17 16:06:48 -0300

    :sparkles: Create Authentication Components

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,19 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext"
 
 function Navbar() {
+
+  const navigate = useNavigate()
+
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
       <div className="container flex justify-between text-lg">
@@ -13,7 +26,7 @@ function Navbar() {
               <li className="mr-4">Temas</li>
               <li className="mr-4">Cadastrar Tema</li>
               <li className="mr-4">Perfil</li>
-              <li className="mr-4"><Link to="/login">Sair</Link></li>
+              <li className="mr-4"><Link to='' onClick={logout} className='hover:underline'>Sair</Link></li>
             </ul>
           </nav>
         </div>
